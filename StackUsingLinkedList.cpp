7381dc8701aee76fdb3c599b5b3e118b713789af
haris49 <hariis.459@gmail.com>
2026-05-12 11:07:05 +0700

    Menambahkan input pilihan menu pada program

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -82,6 +82,9 @@ int main()
         cout << "1. Push" << endl;
         cout << "2. Pop" << endl;
         cout << "3. Peek" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
     }
 
     
